9da85816c5b6a4ad7bb2cfad3e3e9c56fd8499e2
gkenggen <matiasgenesis594@gmail.com>
2025-06-03 01:18:13 +0800

    Delete train_model.py

--- train_model.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-# Save the trained model
-joblib.dump(model, 'model.joblib')
-
-# Save the scaler and label encoder if used
-joblib.dump(scaler, 'scaler.joblib')
-joblib.dump(label_encoder, 'label_encoder.joblib')
